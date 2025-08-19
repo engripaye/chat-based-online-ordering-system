c09be66b725773ee4be9ac6abfaa3a8a4e4f404a
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-19 03:24:02 +0100

    first commit

---
+++ src/main/java/dev/engripaye/chat_based_online_ordering_system/ChatBasedOnlineOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.chat_based_online_ordering_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBasedOnlineOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBasedOnlineOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/engripaye/chat_based_online_ordering_system/ChatBasedOnlineOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.chat_based_online_ordering_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBasedOnlineOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
